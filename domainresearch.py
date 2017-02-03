2c82dca78fea2e05f6479fd12b158c42cbf5a497
Ben Pinkert <bpinkert@websitesavants.com>
2017-02-02 21:36:35 -0500

    fixed ns3. still need to work on registered organization and expiration date

--- domainresearch.py
+++ domainresearch.py
@@ -12,7 +12,10 @@ def registrar(dom):
 	creation = str(d.creation_date)
 	cd = str(creation)
 	cdd = cd.split('u')
-	cdate = cdd[1]
+	if len(cdd) == 2:
+		cdate = cdd[1]
+	else:
+	 	cdate = None
 	update = str(d.update_date)
 	registrar = str(d.registrar)
 	refurl = str(d.referral_url)
@@ -28,6 +31,8 @@ def registrar(dom):
 	ns2 = nameservers[1][3:-1]
 	if len(nameservers) > 2:
 		ns3 = nameservers[2][3:-1]
+	else:
+		ns3 = None
 	org = str(d.org)
 	emails = str(d.emails)
 	elist = emails.split(',')
@@ -39,21 +44,30 @@ def cleanreg(r):
 	"""
 	print "Domain Name: %s\n" % r[0]
 	print "Transfer Status: %s\n" % r[1].split(',')[0][3:-1]
-	print "Creation Date: %s\n" % r[2][1:-2]
+	if r[2] != None:
+		print "Creation Date: %s\n" % r[2][1:-2]
+	else:
+		pass
 	print "Date Updated: %s\n" % r[3]
 	print "Registrar Name: %s\n" % r[4]
 	print "Referral URL: %s\n" % r[5]
 	print "Whois Server: %s\n" % r[6]
 	print "DNSSEC: %s\n" % r[7]
-	print "Registered Organization: %s\n" % r[8].split(',')[1][3:-2] 
-	print "Registered Address: %s\n" % r[8].split(',')[0][3:-1]
+	# if r[8] != None:
+	# 	print "Registered Organization: %s\n" % r[8].split(',')[1][3:-2]
+	# else:
+	# 	pass
+	print "Registered Address: %s\n" % r[8]
 	print "Registered City: %s\n" % r[9]
 	print "Registered State: %s\n" % r[10]
 	print "Registered Zip: %s\n" % r[11]
-	print "Expiration Date: %s\n" % r[12].split('u')[1][1:-3]
+	# print "Expiration Date: %s\n" % r[12].split('u')[1][1:-3]
 	print "Nameserver 1: %s\n" % r[13]
 	print "Nameserver 2: %s\n" % r[14]
-	print "Nameserver 3: %s\n" % r[15][0:-1]
+	if r[15] != None:
+		print "Nameserver 3: %s\n" % r[15][0:-1]
+	else: 
+		pass
 	print "Whois Organization: %s\n" % r[16]
 	print "Email 1: %s\n" % r[17][0][3:-1]
 	print "Email 2: %s\n" % r[17][1][3:-2]

fb57997e515c4262f51598f13e27348ce309e048
Ben Pinkert <bpinkert@websitesavants.com>
2017-02-02 20:41:48 -0500

    got cleanreg function working, need to add argparse

--- urlresearch.py
+++ urlresearch.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 import whois
 
-def lookup(domain):
-	
-	return d
-
-def updated(d):
-	return update
-
-def status(d):
-
 def registrar(dom):
 	d = whois.whois(str(dom))
 	domain = str(d.domain_name)
@@ -28,10 +19,36 @@ def registrar(dom):
 	regstate = str(d.state)
 	regzip = str(d.zipcode)
 	expdate = str(d.expiration_date)
-	nameservers = str(d.name_servers)
+	nameservers = str(d.name_servers).split(',')
+	ns1 = nameservers[0][3:-1]
+	ns2 = nameservers[1][3:-1]
+	if len(nameservers) > 2:
+		ns3 = nameservers[2][3:-1]
 	org = str(d.org)
 	emails = str(d.emails)
-	return domain, transferstat, cdate, update, registrar, refurl, whoserver, dnssec, address, regcity, regstate, regzip, expdate, nameservers, org, emails
+	elist = emails.split(',')
+	return domain, transferstat, cdate, update, registrar, refurl, whoserver, dnssec, address, regcity, regstate, regzip, expdate, ns1, ns2, ns3, org, elist
 
-def cleanreg(domain, transferstat, cdate, update, registrar, refurl, whoserver, dnssec, address, regcity, regstate, regzip, expdate, nameservers, org, emails)
+def cleanreg(r):
+	print "Domain Name: %s\n" % r[0]
+	print "Transfer Status: %s\n" % r[1].split(',')[0][3:-1]
+	print "Creation Date: %s\n" % r[2][1:-2]
+	print "Date Updated: %s\n" % r[3]
+	print "Registrar Name: %s\n" % r[4]
+	print "Referral URL: %s\n" % r[5]
+	print "Whois Server: %s\n" % r[6]
+	print "DNSSEC: %s\n" % r[7]
+	print "Registered Organization: %s\n" % r[8].split(',')[1][3:-2] 
+	print "Registered Address: %s\n" % r[8].split(',')[0][3:-1]
+	print "Registered City: %s\n" % r[9]
+	print "Registered State: %s\n" % r[10]
+	print "Registered Zip: %s\n" % r[11]
+	print "Expiration Date: %s\n" % r[12].split('u')[1][1:-3]
+	print "Nameserver 1: %s\n" % r[13]
+	print "Nameserver 2: %s\n" % r[14]
+	print "Nameserver 3: %s\n" % r[15][0:-1]
+	print "Whois Organization: %s\n" % r[16]
+	print "Email 1: %s\n" % r[17][0][3:-1]
+	print "Email 2: %s\n" % r[17][1][3:-2]
 
+registrar('savantdigital.net')
